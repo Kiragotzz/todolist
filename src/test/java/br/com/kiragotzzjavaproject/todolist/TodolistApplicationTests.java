2bd3337a99bf64088a04ed04160316e9365fee00
Luís Gabriel Gomes da Silva <luis_gabriell1@hotmail.com>
2023-10-09 23:09:49 -0300

    first

---
+++ src/test/java/br/com/kiragotzzjavaproject/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kiragotzzjavaproject.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
