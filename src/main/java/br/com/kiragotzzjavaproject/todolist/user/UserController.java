bd8e3d5ac2ee28425d2460eb127b6e2b7bbff806
Luís Gabriel Gomes da Silva <luis_gabriell1@hotmail.com>
2023-10-10 20:17:13 -0300

    using h2 as database to study

--- src/main/java/br/com/kiragotzzjavaproject/todolist/user/UserController.java
+++ src/main/java/br/com/kiragotzzjavaproject/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.kiragotzzjavaproject.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,10 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-  
+
+  @Autowired
+  private IUserRepository userRepository;
+
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel){
-    System.out.println(userModel.getName());
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if (user != null) {
+      System.out.println("Usuario ja existe");
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario ja existente");
+    }
+
+    var userCreated = this.userRepository.save(userModel);
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 
 }
